82948e3030ad5ae27ae68f085952a28a5e060670
Emanuel Scura <emikscur123@gmail.com>
2024-05-05 11:26:38 +0200

    demo

--- main.c
+++ main.c
@@ -12,42 +12,36 @@
 
 #include "ft_alloc.h"
 
-/*
-
-NOT NORMINETTED (Just for examples)
-
-*/
-
 int	main(void)
 {
+    // init list for allocations
 	t_allocs *lst = ft_calloc(1, sizeof(t_allocs));
 	if (lst == NULL)
 		return (1);
 
 	ft_allocs(lst);
 
+    // allocations with ft_malloc
 	char *str = ft_malloc(455 * sizeof(char));
 	char *str2 = ft_malloc(455 * sizeof(char));
 	char *str3 = ft_malloc(455 * sizeof(char));
-	(void)str;
-	(void)str2;
-	(void)str3;
+
+    // manual free
+	ft_free(str);  
+	ft_free(str2);
+	ft_free(str3);
 
 	int i = 0;
 	while (i < 10)
 	{
-		char *str4 = ft_malloc(455 * sizeof(char));
+		char *str4 = ft_malloc(455 * sizeof(char)); // allocation without ft_free
 		(void)str4;
 		i++;
 	}
 
-	ft_free(str);
-	ft_free(str2);
-	ft_free(str3);
-
-	char *str5 = ft_malloc(455 * sizeof(char));
+	char *str5 = malloc(455 * sizeof(char)); // original malloc - won't be freed with destructor
 	(void)str5;
 
-	ft_destructor();
+	ft_destructor(); // will free everything that was allocated with ft_malloc or ft_calloc
 	return (0);
 }
